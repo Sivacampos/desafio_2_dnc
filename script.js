58c870f4b9240f4eca958b594304eb6eba593390
Sivacampos <sivatecnology.io@gmail.com>
2024-04-22 23:36:43 -0300

    Desafio 2 DNC v5

--- script.js
+++ script.js
@@ -23,9 +23,25 @@ async function getAddressByCep() {
         document.getElementById('uf').value = data.uf;
         }
         catch (error) {
-            alert(error.menssage);
+            alert(error.message);
         }
 }
+
+/*async function getPrevisao() {
+    const lat = document.getElementById('latitude').value;
+    const log = document.getElementById('longitude').value;
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${log}&hourly=temperature_2m`);
+        const data = await response.json();
+        var datas = data.hourly.time.join('\r\n');
+        console.log(data);
+        document.getElementById('resposta').value = datas;
+
+        }
+        catch (error) {
+            alert(error.message);
+        }
+}*/
 
 async function getPrevisao() {
     const lat = document.getElementById('latitude').value;
@@ -34,10 +50,17 @@ async function getPrevisao() {
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${log}&hourly=temperature_2m`);
         const data = await response.json();
         console.log(data);
-        /*document.getElementById('resposta').value = ${data.hourly.time[index]} - ${data.hourly.temperature_2m[index]};*/
+        document.getElementById('resposta').innerHTML = '';
+        for (
+            let index = 0;
+            index < data.hourly.temperature_2m.length;
+            index++) {
+            document.getElementById('resposta').innerHTML += `<div>${data.hourly.time[index]} - ${data.hourly.temperature_2m[index]}</div>`;
+
+        }
 
         }
         catch (error) {
-            alert(error.menssage);
+            alert(error.message);
         }
 }
