3b79b8979f277c78e620452077bf30a8daf5f243
Sivacampos <sivatecnology.io@gmail.com>
2024-04-29 21:19:38 -0300

    Desafio 2 DNC v6

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ async function getAddressByCep() {
         document.getElementById('logradouro').value = data.logradouro;
         document.getElementById('bairro').value = data.bairro;
         document.getElementById('uf').value = data.uf;
-        }
-        catch (error) {
-            alert(error.message);
-        }
+    }
+    catch (error) {
+        alert(error.message);
+    }
 }
 
 /*async function getPrevisao() {
@@ -51,16 +51,39 @@ async function getPrevisao() {
         const data = await response.json();
         console.log(data);
         document.getElementById('resposta').innerHTML = '';
-        for (
+        document.getElementById('resposta').innerHTML += `<div> ${data.hourly.temperature_2m[0]}°C - ${data.hourly.time[0]}</div>`;
+
+        /*for (
             let index = 0;
             index < data.hourly.temperature_2m.length;
             index++) {
             document.getElementById('resposta').innerHTML += `<div>${data.hourly.time[index]} - ${data.hourly.temperature_2m[index]}</div>`;
 
-        }
+        }*/
 
-        }
-        catch (error) {
-            alert(error.message);
-        }
+    }
+    catch (error) {
+        alert(error.message);
+    }
 }
+
+/*async function getPrevisao() {
+    const lat = document.getElementById('latitude').value;
+    const log = document.getElementById('longitude').value;
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${log}&hourly=temperature_2m`);
+        const data = await response.json();
+        console.log(data);
+
+        // Limpa a resposta anterior
+        document.getElementById('resposta').innerHTML = '';
+
+        // Acessa apenas o primeiro elemento do array
+        const primeiroResultado = data.hourly.temperature_2m[0];
+
+        // Exibe o primeiro resultado
+        document.getElementById('resposta').innerHTML = <div>${data.hourly.time[0]}  ${primeiroResultado}</div>;
+    } catch (error) {
+        alert(error.message);
+    }
+} */
